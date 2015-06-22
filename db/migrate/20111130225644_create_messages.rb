07e7d0e683b88afea6b94bb980c5b2fa21af97d1
sagax <sagax@inbox.ru>
2015-06-22 11:43:34 +0300

    fixed model message

--- db/migrate/20111130225644_create_messages.rb
+++ db/migrate/20111130225644_create_messages.rb
@@ -4,7 +4,8 @@ class CreateMessages < ActiveRecord::Migration
       t.string   :who
       t.text     :what
       t.datetime :when
-      t.string   :where
+      t.string   :channel
     end
+    add_index :messages, [:channel, :when]
   end
 end
--- db/migrate/20120203233207_index_messages.rb
+++
@@ -1,5 +0,0 @@
-class IndexMessages < ActiveRecord::Migration
-  def change
-    add_index :messages, [:channel, :when]
-  end
-end
